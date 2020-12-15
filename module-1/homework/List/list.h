22745559d7dbc0394d71d6b14a587f88f8c5fdd0
CardinalPrefect <rumpel135@gmail.com>
2020-12-15 17:58:31 +0300

    The solution of task list

--- module-1/homework/List/list.h
+++ module-1/homework/List/list.h
@@ -3,17 +3,45 @@
 
 
 namespace task {
+class Node {
+public:
+    Node(int value, Node* prev, Node* next):
+        value(value), prev(prev), next(next) { }
 
+    void push_back(Node* node) {
+        next = node;
+        if (node)
+            node->prev = this;
+    }
+
+    void push_front(Node* node) {
+        prev = node;
+        if (node)
+            node->next = this;
+    }
+
+    ~Node() { }
+//private:
+    int value = 0;
+    Node* prev = nullptr;
+    Node* next = nullptr;
+
+    friend class list;
+};
 
 class list {
+
 
 public:
 
     list();
+    list(const list& other);
+    list(list&& other) noexcept;
     list(size_t count, const int& value = int());
 
     ~list();
     list& operator=(const list& other);
+    list& operator=(list&& other) noexcept;
 
 
     int& front();
@@ -40,11 +68,10 @@ public:
     void unique();
     void sort();
 
-    // Your code goes here?..
-
-private:
-
-    // Your code goes here...
+//private:
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    std::size_t list_size = 0;
 
 };
 
