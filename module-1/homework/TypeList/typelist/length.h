bec4d533746f995cca9c3bdea0fa79b8ae1c559f
CardinalPrefect <rumpel135@gmail.com>
2020-12-23 17:18:06 +0300

    The Solution of typelist task

--- module-1/homework/TypeList/typelist/length.h
+++ module-1/homework/TypeList/typelist/length.h
@@ -1,6 +1,17 @@
 #pragma once
 
 #include "typelist.h"
+#include <iostream> // for size_t
 
-template<typename TList> 
+template<typename TList>
 struct Length;
+
+template <typename Head, typename Tail>
+struct Length<TypeList<Head, Tail> > {
+    static const std::size_t length = 1 + Length<Tail>::length;
+};
+
+template<>
+struct Length<NullType> {
+    static const int length = 0;
+};
